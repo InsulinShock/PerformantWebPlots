bbdfa9fb3cbfda188bddecabecf01ad9654ea377
InsulinShock <gmcvitti@gmail.com>
2019-10-30 08:57:45 -0400

    Seems to be working, not sure about exact sequence but looks like progress.

--- example1/main.cpp
+++ example1/main.cpp
@@ -9,6 +9,30 @@
 #include <memory>
 #include <GLES3/gl32.h>
 
+
+EMSCRIPTEN_WEBGL_CONTEXT_HANDLE context;
+
+void main_loop(void) 
+{
+    emscripten_webgl_make_context_current(context);   
+    glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+    glClearStencil(0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    glFinish();
+
+
+    emscripten::val onScreenCanvas = emscripten::val::global("document").call<emscripten::val>("querySelector",std::string("#canvas"));
+    emscripten::val offScreenCanvas = emscripten::val::global("document").call<emscripten::val>("querySelector",std::string("#offscreen"));
+
+    onScreenCanvas
+        .call<emscripten::val>("getContext",std::string("2d"))
+        .call<void>("drawImage",offScreenCanvas, emscripten::val(0), emscripten::val(0));
+
+};
+
+
+
+
 int main()
 {
     EmscriptenWebGLContextAttributes attr;
@@ -25,17 +49,26 @@ int main()
     attr.majorVersion = 3;
     attr.minorVersion = 2;
 
-    EMSCRIPTEN_WEBGL_CONTEXT_HANDLE context = emscripten_webgl_create_context("#offscreen", &attr);
+    context = emscripten_webgl_create_context("#offscreen", &attr);
 
-    emscripten_webgl_make_context_current(context);
-
-    std::cout << glGetString(GL_VERSION) << std::endl;
-
-    emscripten::val canvas = emscripten::val::global("document").call<emscripten::val>("getElementById",std::string("canvas"));
-
+    emscripten_webgl_make_context_current(context);   
     glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
     glClearStencil(0);
     glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    glFinish();
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    emscripten::val onScreenCanvas = emscripten::val::global("document").call<emscripten::val>("querySelector",std::string("#canvas"));
+    emscripten::val offScreenCanvas = emscripten::val::global("document").call<emscripten::val>("querySelector",std::string("#offscreen"));
+
+
+
+    onScreenCanvas
+        .call<emscripten::val>("getContext",std::string("2d"))
+        .call<void>("drawImage",offScreenCanvas, emscripten::val(0), emscripten::val(0));
+
+    emscripten_set_main_loop(main_loop,0,1);
 
     return 0;
 };
